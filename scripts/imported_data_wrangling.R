b81fffc7b3e6213bb0602c8a954033653e206607
AlvinSheng <asheng1067@gmail.com>
2021-06-10 09:25:18 -0500

    updating the FIPS code for Oglala Lakota County in the CDC SVI dataset

--- scripts/imported_data_wrangling.R
+++ scripts/imported_data_wrangling.R
@@ -6,11 +6,15 @@ library(tidyverse)
 
 i_am("scripts/imported_data_wrangling.R")
 
+
+
 # reading in the county flood risk data
 flood_risk <- read.csv(here("imported_data", "flood_risk", "County_level_risk_FEMA_FSF_v1.3.csv"))
 
 # Uppercase the fips
 flood_risk <- rename(flood_risk, FIPS = fips)
+
+
 
 # reading in the Life Expectancy/Mortality Risk data
 # omitting the last two rows, which don't have data
@@ -31,10 +35,19 @@ for (j in 3:11) {
 saveRDS(life_expect_mort_no_ui, file = here("imported_data", "life_expectancy_mortality_risk", 
                                          "life_expect_mort_no_ui.rds"))
 
+
+
 # reading in the CDC SVI data
 cdc_svi <- read.csv(here("imported_data", "CDC_SVI", "SVI2014_US_CNTY.csv"))
 
+# remove empty variable FID
 cdc_svi <- subset(cdc_svi, select = -FID)
+
+# Because the CDC SVI data was created in 2014, the data for Shannon County, South Dakota
+# is now data for Oglala Lakota County, South Dakota.
+# Thus, the FIPS for the county needs to be changed from 46113 to 46102.
+
+cdc_svi$FIPS[cdc_svi$FIPS == 46113] <- 46102
 
 
 
@@ -44,7 +57,23 @@ flood_le <- merge(flood_risk, life_expect_mort_no_ui, all.x = T, by = "FIPS")
 
 flood_le_svi <- merge(flood_le, cdc_svi, all.x = T, by = "FIPS")
 
+
+
+# remove redundant columns, move id columns to the left
+
+
+
 saveRDS(flood_le_svi, file = here("intermediary_data/flood_le_svi.rds"))
+
+
+
+# working with the countyadj.csv, downloaded from 
+# https://github.com/abhirupdatta/UScounties
+
+countyadj <- read.csv(here("imported_data", "countyadj.csv"), header = T, row.names = 1)
+
+
+
 
 
 

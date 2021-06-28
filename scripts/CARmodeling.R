1c327925451f5e5e224ab1d261b9b449afeebbad
AlvinSheng <asheng1067@gmail.com>
2021-06-28 15:12:43 -0400

    committing the initial version of model1

--- scripts/CARmodeling.R
+++ scripts/CARmodeling.R
@@ -1,5 +1,6 @@
 
 library(CARBayes)
+library(here)
 
 # see spatial_markov_model_sim.Rmd for example CARBayes code
 
@@ -41,13 +42,16 @@ X[is.na(X)] <- 0        # Fill in missing values with the mean
 
 tick <- proc.time()[3]
 
-model1  <- S.CARleroux(Y ~ X, family="gaussian", W=W, burnin = 10, n.sample = 50, thin = 1, verbose=TRUE)
+model1  <- S.CARleroux(Y ~ X, family="gaussian", W=W, burnin = 10000,
+                       n.sample = 100000, thin = 5, verbose = TRUE)
 
 tock <- proc.time()[3]
 
 (tock-tick)/60 # time in minutes
 
 
+
+save(model1, file = here("modeling_files/fls_model_df.rds"))
 
 
 
@@ -70,42 +74,42 @@ tock <- proc.time()[3]
 
 
 
-#################################################
-#### Run the model on simulated data on a lattice
-#################################################
-#### Load other libraries required
-library(MASS)
-
-#### Set up a square lattice region
-x.easting <- 1:10
-x.northing <- 1:10
-Grid <- expand.grid(x.easting, x.northing)
-K <- nrow(Grid)
-
-#### set up distance and neighbourhood (W, based on sharing a common border) matrices
-distance <- as.matrix(dist(Grid))
-W <-array(0, c(K,K))
-W[distance==1] <-1 	
-
-#### Generate the covariates and response data
-x1 <- rnorm(K)
-x2 <- rnorm(K)
-theta <- rnorm(K, sd=0.05)
-phi <- mvrnorm(n=1, mu=rep(0,K), Sigma=0.4 * exp(-0.1 * distance))
-logit <- x1 + x2 + theta + phi
-prob <- exp(logit) / (1 + exp(logit))
-trials <- rep(50,K)
-Y <- rbinom(n=K, size=trials, prob=prob)
-
-
-#### Run the Leroux model
-formula <- Y ~ x1 + x2
-# ## Not run: model <- S.CARleroux(formula=formula, family="binomial",
-# trials=trials, W=W, burnin=20000, n.sample=100000)
-# ## End(Not run)
-
-#### Toy example for checking
-model <- S.CARleroux(formula=formula, family="binomial", 
-                     trials=trials, W=W, burnin=10, n.sample=50)
-
+# #################################################
+# #### Run the model on simulated data on a lattice
+# #################################################
+# #### Load other libraries required
+# library(MASS)
+# 
+# #### Set up a square lattice region
+# x.easting <- 1:10
+# x.northing <- 1:10
+# Grid <- expand.grid(x.easting, x.northing)
+# K <- nrow(Grid)
+# 
+# #### set up distance and neighbourhood (W, based on sharing a common border) matrices
+# distance <- as.matrix(dist(Grid))
+# W <-array(0, c(K,K))
+# W[distance==1] <-1 	
+# 
+# #### Generate the covariates and response data
+# x1 <- rnorm(K)
+# x2 <- rnorm(K)
+# theta <- rnorm(K, sd=0.05)
+# phi <- mvrnorm(n=1, mu=rep(0,K), Sigma=0.4 * exp(-0.1 * distance))
+# logit <- x1 + x2 + theta + phi
+# prob <- exp(logit) / (1 + exp(logit))
+# trials <- rep(50,K)
+# Y <- rbinom(n=K, size=trials, prob=prob)
+# 
+# 
+# #### Run the Leroux model
+# formula <- Y ~ x1 + x2
+# # ## Not run: model <- S.CARleroux(formula=formula, family="binomial",
+# # trials=trials, W=W, burnin=20000, n.sample=100000)
+# # ## End(Not run)
+# 
+# #### Toy example for checking
+# model <- S.CARleroux(formula=formula, family="binomial", 
+#                      trials=trials, W=W, burnin=10, n.sample=50)
+# 
 

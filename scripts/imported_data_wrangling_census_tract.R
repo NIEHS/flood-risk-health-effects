187d6caab8a04665a591938c3b82bcf9b8877958
AlvinSheng <asheng1067@gmail.com>
2021-07-06 10:12:58 -0400

    uploading updated census tract wrangling, for NC census tracts

--- scripts/imported_data_wrangling_census_tract.R
+++ scripts/imported_data_wrangling_census_tract.R
@@ -20,6 +20,8 @@ flood_risk <- read.csv(here("imported_data", "flood_risk", "Zip_level_risk_FEMA_
 # TBC: focusing on county-level flood risk for now. I can map zip code to census tracts later, or just wait till we can process the 3m raster data. 
 # reading in the county flood risk data
 flood_risk <- read.csv(here("imported_data", "flood_risk", "County_level_risk_FEMA_FSF_v1.3.csv"))
+# rename fips into CountyFIPS
+flood_risk <- rename(flood_risk, CountyFIPS = fips)
 
 
 
@@ -41,7 +43,15 @@ places_dat_wide <- pivot_wider(places_subset, id_cols = c(LocationID, CountyFIPS
                                names_from = MeasureId, 
                                values_from = c(Data_Value, Low_Confidence_Limit, High_Confidence_Limit))
   
-  
+places_dat_wide <- rename(places_dat_wide, fips = LocationID)
+
+# TBC: focusing on NC census tracts for now
+
+places_dat_wide <- places_dat_wide[places_dat_wide$fips %/% 1e9 == 37, ]
+
+
+
+
 
 # reading in the CDC SVI data
 cdc_svi <- read.csv(here("imported_data", "CDC_SVI", "SVI2018_US.csv"))
@@ -52,7 +62,9 @@ cdc_svi <- rename(cdc_svi, fips = FIPS)
 # take care of the -999 missing value indicators
 cdc_svi[cdc_svi == -999] <- NA
 
+# TBC: focusing on NC census tracts for now
 
+cdc_svi <- cdc_svi[cdc_svi$fips %/% 1e9 == 37, ]
 
 
 
@@ -64,38 +76,38 @@ cdc_svi[cdc_svi == -999] <- NA
 
 # CACES LUR air pollution data
 
-# Extracting the list of county fips in the dataset, for CACES data extraction
-
-fips <- as.character(places_dat_wide$LocationID)
-
-# TBC: fips just for North Carolina
-fips <- as.character(places_dat_wide$LocationID[places_dat_wide$LocationID %/% 1e9 == 37])
-
-# TBC: figure out what happens to census tracts in Oglala County
-# # switch fip for Oglala County, since CACES uses outdated fips
-# fips[fips == 46102] <- 46113
-
-fips_leading_zero <- sapply(fips, FUN = function(fip) {
-  if (str_length(fip) == 10) {paste0("0", fip)}
-  else {fip}
-})
-
-# TBC: list of fips just for North Carolina
-write.csv(fips_leading_zero, file = here("intermediary_data/census_tract_fips_NC.txt"),
-          row.names = FALSE)
-
-# CACES doesn't like files with more than 5k fips
-write.csv(fips_leading_zero, file = here("intermediary_data/census_tract_fips.txt"),
-          row.names = FALSE)
+# # Extracting the list of county fips in the dataset, for CACES data extraction
+# 
+# fips <- as.character(places_dat_wide$fips)
+# 
+# # TBC: fips just for North Carolina
+# fips <- as.character(places_dat_wide$fips[places_dat_wide$fips %/% 1e9 == 37])
+# 
+# # TBC: figure out what happens to census tracts in Oglala County
+# # # switch fip for Oglala County, since CACES uses outdated fips
+# # fips[fips == 46102] <- 46113
+# 
+# fips_leading_zero <- sapply(fips, FUN = function(fip) {
+#   if (str_length(fip) == 10) {paste0("0", fip)}
+#   else {fip}
+# })
+# 
+# # TBC: list of fips just for North Carolina
+# write.csv(fips_leading_zero, file = here("intermediary_data/census_tract_fips_NC.txt"),
+#           row.names = FALSE)
+# 
+# # CACES doesn't like files with more than 5k fips
+# write.csv(fips_leading_zero, file = here("intermediary_data/census_tract_fips.txt"),
+#           row.names = FALSE)
 
 # reading in the downloaded data from
-# https:
+# https://s3.amazonaws.com/files.airclimateenergy.org/caces/uwc162557682462152e2a39746642243d77e984d07dd562d.zip
 
-caces_lur <- read.csv(here("imported_data/caces_lur_air_pollution/caces_lur_air_pollution.csv"))
+caces_lur <- read.csv(here("imported_data/caces_lur_air_pollution/caces_lur_air_pollution_NC_census_tract.csv"))
 
 # don't need the year, state_abbr, lat or lon
 
-caces_lur_subset <- select(caces_lur, -c(year, state_abbr, lat, lon))
+caces_lur_subset <- dplyr::select(caces_lur, -c(year, state_abbr, lat, lon))
 
 # convert from long to wide format
 
@@ -107,33 +119,28 @@ caces_lur_wide <- spread(caces_lur_subset, pollutant, pred_wght)
 
 # merge all three datasets together by their fips
 
-# merging life expectancy with flood risk
-flood_le <- merge(life_expect_mort_no_ui, flood_risk, all.x = T, by = "fips")
+# merging health outcomes with flood risk
+flood_health <- merge(places_dat_wide, flood_risk, all.x = T, by = "CountyFIPS")
 
 # then merging with SVI 
-flood_le_svi <- merge(flood_le, cdc_svi, all.x = T, by = "fips")
+flood_health_svi <- merge(flood_health, cdc_svi, all.x = T, by = "fips")
 
 # then merging with air pollution
-flood_le_svi <- merge(flood_le_svi, caces_lur_wide, all.x = T, by = "fips")
-
-# then merging with smoke prevalence
-flood_le_svi <- merge(flood_le_svi, smoke_fips, all.x = T, by = "fips")
+flood_health_svi <- merge(flood_health_svi, caces_lur_wide, all.x = T, by = "fips")
 
 
-
-# remove geographical units that are not counties
-
-flood_le_svi <- flood_le_svi[!is.na(flood_le_svi$COUNTY), ] 
 
 # remove counties in Alaska and Hawaii
 
-flood_le_svi <- flood_le_svi[!(flood_le_svi$STATE %in% c("ALASKA", "HAWAII")), ]
+flood_health_svi <- flood_health_svi[!(flood_health_svi$STATE %in% c("ALASKA", "HAWAII")), ]
 
 
 
 # save the dataset
 
-saveRDS(flood_le_svi, file = here("intermediary_data/flood_le_svi.rds")) # TBC take the chance to rename into smth better
+# TBC: NC census tracts for now
+
+saveRDS(flood_health_svi, file = here("intermediary_data/flood_health_svi_NC_census_tracts.rds")) 
 
 
 
@@ -141,11 +148,11 @@ saveRDS(flood_le_svi, file = here("intermediary_data/flood_le_svi.rds")) # TBC t
 
 # Removing redundant columns, moving id columns to the left
 
-flood_le_svi <- readRDS(file = here("intermediary_data/flood_le_svi.rds"))
+flood_health_svi <- readRDS(file = here("intermediary_data/flood_health_svi_NC_census_tracts.rds"))
 
 # remove life expectancy variables other than "Life expectancy, 2014*"
 # this also puts the outcome variable as the last variable
-fls_outcome_subset <- flood_le_svi %>% dplyr::select(!starts_with("Life expectancy") | ends_with(", 2014*")) %>%
+fls_outcome_subset <- flood_health_svi %>% dplyr::select(!starts_with("Life expectancy") | ends_with(", 2014*")) %>%
   dplyr::select(!`% Change in Life Expectancy, 1980-2014`)
 
 # Deleting and reorganizing some flood risk variables

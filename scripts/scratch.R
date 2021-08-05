1907f2a68d94e9cec972024317ffa6be1ca5887f
AlvinSheng <asheng1067@gmail.com>
2021-08-05 11:41:35 -0400

    uploading pdf of before CAR model analysis for the model presented in poster

--- scripts/scratch.R
+++ scripts/scratch.R
@@ -12,3 +12,15 @@ for (i in 1:nrow(countyadj)) {
   
 }
 
+
+
+fl_count_prop <- data.frame(flood_risk_prev$fips, flood_risk_prev$count_property)
+head(fl_count_prop)
+fl_count_prop <- data.frame(fips = flood_risk_prev$fips, fl_count_prop = flood_risk_prev$count_property)
+head(fl_count_prop)
+cdc_count_hu <- data.frame(fips = cdc_svi$fips, cdc_count_hu = cdc_svi$E_HU)
+head(cdc_count_hu)
+fl_cdc_hu <- left_join(fl_count_prop, cdc_count_hu, by = "fips")
+plot(fl_cdc_hu$fl_count_prop, fl_cdc_hu$cdc_count_hu)
+
+

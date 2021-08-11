a76f8dfae4f67bd97ec65f770da13969a120ad67
AlvinSheng <asheng1067@gmail.com>
2021-08-11 11:23:02 -0400

    working on MCMC implementation of CAR model

--- scripts/my_gaussian_leroux_car.R
+++ scripts/my_gaussian_leroux_car.R
@@ -2,22 +2,23 @@
 
 
 #' Function to compute the log posterior for the spatial markov model M-H steps
+#' Just used for parameter rho
 #'
-#' @param y_mat response variables
-#' @param parameters: Z_mat, W, num_nbr_mat, sigma2, rho, mu, tau2, cluster_labs
+#' @param y response variable
+#' @param parameters: Z, W, num_nbr_mat, sigma2, rho, mu, tau2, X
 #' later on this will involve M, phi
 #' @return log of the posterior density 
 #' @export
-log_post_agdsq <- function(y_mat, Z_mat, W, num_nbr_mat, sigma2, rho,
-                           mu, tau2, cluster_labs) {
+log_post_agdsq <- function(y, Z, W, num_nbr_mat, sigma2, rho,
+                           mu, tau2, X) {
   
-  num_gp <- dim(y_mat)[2]
+  n <- length(y)
   
-  like <- sum(dnorm(y_mat, mu[cluster_labs] + Z_mat, tau2, log = TRUE))
+  like <- sum(dnorm(y, X %*% mu + Z, tau2, log = TRUE))
   
   # spatial terms
-  Sigma <- sigma2 * solve(num_nbr_mat - rho * W)
-  spatial_like <- sum(dmvnorm(Z_mat, mean = rep(0, num_gp), sigma = Sigma, log = T))
+  Sigma <- sigma2 * solve((1 - rho) * diag(n) + rho * (num_nbr_mat - W))
+  spatial_like <- sum(dmvnorm(Z, mean = rep(0, n), sigma = Sigma, log = T))
   
   # cluster means
   cluster_mean_like <- sum(dnorm(mu, 0, tau2 * b, log = TRUE))
@@ -41,78 +42,42 @@ log_post_agdsq <- function(y_mat, Z_mat, W, num_nbr_mat, sigma2, rho,
 
 #' One MCMC iteration of the Metropolis algorithm for the spatial markov model
 #'
-#' @param y_mat response variables
+#' @param y response variable
 #' @param log_post the log posterior of the last iteration
-#' @param parameters: Z_mat, W, num_nbr_mat, sigma2, rho, mu, tau2, cluster_labs
+#' @param parameters: Z, W, num_nbr_mat, sigma2, rho, mu, tau2, X
 #' @param can_sd list containing the candidate standard deviations for 
 #' @return the updated parameters for the MCMC iteration
 #' @export
-met_glm_iter <- function(y_mat,
+met_glm_iter <- function(y,
                          log_post,
-                         Z_mat, W, num_nbr_mat, sigma2, rho, mu, tau2, cluster_labs,
+                         Z, W, num_nbr_mat, sigma2, rho, mu, tau2, X,
                          can_sd) {
   
-  # Candidate sd's for alpha and delta
+  # Candidate sd's for rho, ...
   
-  can_alpha_sd <- can_sd$can_alpha_sd
-  
-  can_delta_sd <- can_sd$can_delta_sd
-  
-  can_sigma_sd <- can_sd$can_sigma_sd
+  can_rho_sd <- can_sd$can_rho_sd 
   
   
   
-  # Metropolis/Gibbs for Z_mat, sigma2, rho, mu, tau2
+  # Metropolis/Gibbs for Z, sigma2, rho, mu, tau2
   
-  # Z_mat
-  
-  for (i in 1:n) { # subject
-    
-    for (j in 1:num_gp) { # grid point
-      
-      can_Z_mat <- Z_mat
-      can_Z_mat[i, j] <- rnorm(1, Z_mat[j], can_Z_mat_sd[j])
-      cans <- Xb_beta_update(X, Xb, gamma, can_Z_mat, beta, j, can = "delta")
-      can_Xb <- cans$can_Xb
-      can_log_post <- log_post_agdsq(Y, can_Xb, alpha, gamma, can_Z_mat, sigma, q)
-      logR <- can_log_post - log_post
-      if (log(runif(1)) < logR) {
-        Z_mat <- can_Z_mat
-        beta <- cans$can_beta
-        Xb <- can_Xb
-        log_post <- can_log_post
-      }
-      
-    }
-    
-  } 
+  # mu
   
   
   
-  # alpha
-  can_alpha <- rnorm(1, alpha, can_alpha_sd)
-  can_log_post <- log_post_agdsq(Y, Xb, can_alpha, gamma, delta, sigma, q)
-  logR <- can_log_post - log_post
-  if (log(runif(1)) < logR) {
-    alpha <- can_alpha
-    log_post <- can_log_post
-  }
+  # fc.precision <- prior.precision.beta + data.precision.beta / nu2
+  # fc.var <- solve(fc.precision)
+  # beta.offset <- as.numeric(Y.DA - offset - phi)
+  # beta.offset2 <- t(X.standardised) %*% beta.offset / nu2 + prior.precision.beta %*% prior.mean.beta
+  # fc.mean <- fc.var %*% beta.offset2
+  # chol.var <- t(chol(fc.var))
+  # beta <- fc.mean + chol.var %*% rnorm(p)   
   
-  # gamma
-  for (j in 1:num_gamma) {
-    can_gamma <- gamma
-    can_gamma[j] <- 1 - can_gamma[j]
-    cans <- Xb_beta_update(X, Xb, can_gamma, delta, beta, j, can = "gamma")
-    can_Xb <- cans$can_Xb
-    can_log_post <- log_post_agdsq(Y, can_Xb, alpha, can_gamma, delta, sigma, q)
-    logR <- can_log_post - log_post
-    if (log(runif(1)) < logR) {
-      gamma <- can_gamma
-      beta <- cans$can_beta
-      Xb <- can_Xb
-      log_post <- can_log_post
-    }
-  }
+  
+  
+  
+  
+  
   
   # delta
   for (j in 1:p) {

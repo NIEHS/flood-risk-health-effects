d8d89ebf767b2f1bee4a762f02b3602dac53cf9e
AlvinSheng <asheng1067@gmail.com>
2021-10-26 08:50:31 -0400

    15/1000 of the eigenvalues found.

--- scripts/calc_eigs.R
+++ scripts/calc_eigs.R
@@ -3,7 +3,7 @@ library(here)
 
 tick <- proc.time()[3]
 
-Wstar.eigen <- eigs_sym(Wstar, round(nrow(Wstar) * 99/100), opts = list(retvec = FALSE))
+Wstar.eigen <- eigs_sym(Wstar, round(nrow(Wstar) * 1/25), opts = list(retvec = FALSE))
 
 tock <- proc.time()[3]
 
@@ -11,3 +11,9 @@ tock <- proc.time()[3]
 
 
 saveRDS(Wstar.eigen, file = here("intermediary_data/eig_vals.rds"))
+
+
+
+# 18.45 mins to find 1/100 of the eigenvalues
+
+# 32 minutes to find 1.5/100 of the eigenvalues

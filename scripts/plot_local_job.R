409120acd364b645a958bb3cabfd95c4824a31ff
AlvinSheng <asheng1067@gmail.com>
2022-03-13 15:45:21 -0400

    committing the figure plotting

---
+++ scripts/plot_local_job.R
@@ -0,0 +1,74 @@
+library(here)
+library(coda)
+library(CARBayes)
+library(ggplot2)
+library(tidyverse)
+
+
+
+fhs_model_df <- readRDS(here("intermediary_data/fhs_model_df_all_census_tract_pc.rds"))
+
+
+
+load(here("modeling_files/all_census_tract_MHLTH.RData"))
+
+
+
+beta_samples_matrix <- rbind(chain1$samples$beta, chain2$samples$beta, chain3$samples$beta)
+
+colnames(beta_samples_matrix) <- c("Intercept", names(fhs_model_df[, 14:(ncol(fhs_model_df) - 4)]))
+
+
+
+
+
+(beta_inference <- round(t(apply(beta_samples_matrix, 2, quantile, c(0.5, 0.025, 0.975))),5))
+
+
+
+colnames(beta_samples_matrix)[sign(beta_inference[, 2]) == sign(beta_inference[, 3])]
+
+
+
+
+# first, process the beta_inference matrix in a form ggplot can understand
+
+beta_inference_df <- as.data.frame(beta_inference)
+
+beta_inference_df <- mutate(beta_inference_df, var_name = row.names(beta_inference_df))
+
+beta_inference_df <- rename(beta_inference_df, 
+                            post_median = `50%`,
+                            post_2.5 = `2.5%`, 
+                            post_97.5 = `97.5%`)
+
+beta_inference_df$var_name <- factor(beta_inference_df$var_name, levels = beta_inference_df$var_name)
+
+
+
+
+p <- ggplot(beta_inference_df[-1, ], aes(x = var_name, y = post_median)) + 
+  geom_point() + 
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1), axis.title.x = element_blank(), axis.title.y = element_blank(), 
+        axis.text=element_text(size=12), 
+        plot.margin = margin(5.5, 5.5, 5.5, 10)) +
+  geom_errorbar(aes(ymin = post_2.5, ymax = post_97.5, width = 0.4)) + 
+  geom_vline(xintercept = c(4.5, 20.5, 26.5, 30.5), col = "blue") +
+  geom_hline(yintercept = 0, col = "red") +
+  annotate(geom = "text", x = 2.5, y = 1.45, label = "Flood\nRisk",
+           col = "blue", size = 4.5) +
+  annotate(geom = "text", x = 12.5, y = 1.5, label = "Social Vulnerability Index",
+           col = "blue", size = 4.5) +
+  annotate(geom = "text", x = 23.5, y = 1.5, label = "Air Pollution",
+           col = "blue", size = 4.5) +
+  annotate(geom = "text", x = 28.5, y = 1.5, label = "GRIDMET",
+           col = "blue", size = 4.5) +
+  scale_x_discrete(labels = c("PC 1", "PC 2", "PC 3", "PC 4",
+                              "Poverty", "Unemployed", "Per Capita Income", "No High School",
+                              "65 or Over", "17 or Under", "Disability",
+                              "Single-Parent", "Minority", "Poor English",
+                              "Multi-Unit", "Mobile", "Crowded",
+                              "No Vehicle", "Group Quarters", "Uninsured",
+                              "CO", "NO2", "O3", "PM10", "PM2.5", "SO2",
+                              "Summer Temperature", "Winter Temperature", "Summer Humidity", "Winter Humidity",
+                              "Smoking")) + ggtitle("95% Credible Intervals of Coefficients, Poor Mental Health")

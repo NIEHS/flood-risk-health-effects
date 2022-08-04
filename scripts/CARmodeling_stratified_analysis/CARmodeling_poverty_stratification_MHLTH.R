a85e7b4a99e6635781a261d4557efe7633d6b13b
AlvinSheng <asheng1067@gmail.com>
2022-08-04 18:13:46 -0400

    changing the intermediary data read in

--- scripts/CARmodeling_stratified_analysis/CARmodeling_poverty_stratification_MHLTH.R
+++ scripts/CARmodeling_stratified_analysis/CARmodeling_poverty_stratification_MHLTH.R
@@ -12,7 +12,7 @@ select <- dplyr::select
 
 W <- readRDS(here("intermediary_data", "census_tract_adj_reorganize_all_census_tract.rds"))
 
-fhs_model_df <- readRDS(here("intermediary_data/fhs_model_df_all_census_tract_pc.rds"))
+fhs_model_df <- readRDS(here("intermediary_data/fhs_model_df_fr_and_pollute_pc.rds"))
 
 # remove 3 response variables that are not MHLTH
 fhs_model_df <- fhs_model_df[, -(ncol(fhs_model_df) + c(-3, -2, -1))]
